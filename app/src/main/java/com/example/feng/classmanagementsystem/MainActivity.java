9aaf835358e000c269d5cc1b72e0490263503fa2
bs <bs>
2018-04-15 13:30:56 +0800

    bs test1

--- app/src/main/java/com/example/feng/classmanagementsystem/MainActivity.java
+++ app/src/main/java/com/example/feng/classmanagementsystem/MainActivity.java
@@ -11,6 +11,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         int i =1;
-        Log.d("MainActivity","xxxx");
+        Log.d("MainActivity","xxx111x");
     }
 }
